4bca78450d2b5f4906e854c9f6f6c5afa9c80297
Erik Heen <erik@heen.dev>
2023-04-09 16:41:03 -0500

    fix: fish themes, orchis symlinks, gtk.sh style

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	theme.sh nord
 end
 
 if type -q theme.sh
@@ -9,25 +10,25 @@ if type -q theme.sh
 
 	# Optional
 	# Bind C-o to the last theme.
-	function last_theme
-		theme.sh (theme.sh -l|tail -n2|head -n1)
-	end
+	#function last_theme
+	#	theme.sh (theme.sh -l|tail -n2|head -n1)
+	#end
 
-	bind \co last_theme
+	#bind \co last_theme
 
-	alias th='theme.sh -i'
+	#alias th='theme.sh -i'
 
 	# Interactively load a light theme
 	alias thl='theme.sh --light -i'
 
 	# Interactively load a dark theme
 	alias thd='theme.sh --dark -i'
+	
 end
 
 # variables
 
 set -U fish_greeting
-theme.sh nord
 
 set GH ~/code/src/github.com
 set FISH_CFG ~/.config/fish/config.fish
--- gtk.sh
+++ gtk.sh
@@ -1,42 +1,50 @@
-### GNOME shell extensions ###
-gnome-extensions install \
-	https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v50.shell-extension.zip
-gnome-extensions install \
-	https://extensions.gnome.org/extension-data/caffeinepatapon.info.v46.shell-extension.zip
-gnome-extensions install \
-	https://extensions.gnome.org/extension-data/rounded-window-cornersyilozt.v11.shell-extension.zip
+# Set up submodules
+git submodule init
+git submodule update
 
-# enable / disable shell extensions
+
+### GNOME shell extensions ###
+# Download & Install extensions
+
+gnome-extensions install \
+https://extensions.gnome.org/extension-data/user-themegnome-shell-extensions.gcampax.github.com.v50.shell-extension.zip
+
+gnome-extensions install \
+https://extensions.gnome.org/extension-data/caffeinepatapon.info.v46.shell-extension.zip
+
+gnome-extensions install \
+https://extensions.gnome.org/extension-data/rounded-window-cornersyilozt.v11.shell-extension.zip
+
+# Disable default extensions
 gnome-extensions disable background-logo@fedorahosted.org
 
+# Enable extensions
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable apps-menu@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
 gnome-extensions enable caffeine@patapon.info
 
 
-
-# Set up submodules
-git submodule init
-git submodule update
-
-
-### GNOME shell theme ###
-# Install Orchis
+### GNOME shell theme ### [Orchis]
+# Install GTK theme
 chmod +x ./gtk/Orchis/install.sh
-./gtk/Orchis/install.sh -t default -c standard -s compact --tweaks nord -l
+./gtk/Orchis/install.sh -t default -c dark standard -s compact --tweaks nord -l
+unlink ~/.config/gtk-4.0/gtk-dark.css && ln -s ~/.themes/Orchis-Dark-Compact-Nord/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/
 
 # Set GTK theme
 gsettings set org.gnome.desktop.interface gtk-theme Orchis-Compact-Nord
 
 
-### Icons ###
+### Icons ### [Papirus]
+# Install icon theme
 wget -qO- https://git.io/papirus-icon-theme-install | sh
+
+# Set icon theme
 gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
 
 
-### Cursors ###
-# Set up Nordzy cursor theme
+### Cursors ### [Nordzy]
+# Install cursor theme
 chmod +x ./cursors/Nordzy/install.sh
 # ./cursors/Nordzy/install.sh
 cd cursors/Nordzy
